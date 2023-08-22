7c54b373db85ca5e965da56d71c3d9246d1f94d6
Gabriel <gabrielxavierfritsch13@hotmail.com>
2023-08-22 17:26:23 -0300

    voltando o código

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Fiz uma alteração dem você ver!");
 console.log("Outro para fazer o teste");
-
-const frutas = Array('banana', 'maça', 'abacaxi');
-
-console.log(frutas[2]);
