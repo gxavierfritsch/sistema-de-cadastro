757bbc8d25cd8ba404f35b45cf560f769a69bdb4
Gabriel <gabrielxavierfritsch13@hotmail.com>
2023-08-22 17:06:18 -0300

    adicionado um array a aplicação

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Fiz uma alteração dem você ver!");
 console.log("Outro para fazer o teste");
+
+const frutas = Array('banana', 'maça', 'abacaxi');
+
+console.log(frutas[2]);
