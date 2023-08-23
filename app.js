47c3000f3a89cd889c9e490505665399c44368ad
Gabriel <gabrielxavierfritsch13@hotmail.com>
2023-08-23 17:01:58 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Fiz uma alteração dem você ver!");
 console.log("Outro para fazer o teste");
+
+console.log("Bem-vindos ao meu git!");
